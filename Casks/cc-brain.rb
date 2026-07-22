a09ffeb7f3ec61f8f02beac71e06f4b8d15768e4
basheer421 <basheer123b2@gmail.com>
2026-07-22 12:23:22 +0400

    Fix cc-brain SHA after release recreate

--- Casks/cc-brain.rb
+++ Casks/cc-brain.rb
@@ -1,6 +1,6 @@
 cask "cc-brain" do
   version "1.0.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "f22e4c60c70fe55e962db81b38385ba28039be74d6779ab4dfefb20d5bef53e6"
 
   url "https://github.com/basheer421/cc-brain/releases/download/v#{version}/CC-Brain.app.zip"
   name "CC Brain"
